70c4ce9c2fa762b45d1bafcb17413eb461240fcb
Quantum-Anomaly <veneziano82@gmail.com>
2020-11-01 11:28:04 -0500

    Chapter 7- Complete/working -Validations

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,31 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+
+import logging
+import os
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+    mail_handler = SMTPHandler (
+        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+        fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+        toaddrs=app.config['ADMINS'], subject='BAGLFailure',
+            credentials=auth, secure=secure)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/bagl.log', maxBytes=10240, backupCount=10)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('BAGL startup')
